b7b1da3d987db1ca17e28e3a04db4df494547d62
Petr Kozelek <petr.kozelek@gmail.com>
2012-07-21 03:22:28 +0200

    Added "New BSD License".

--- src/main/scala/mql/model/semantic/AliasedColumn.scala
+++ src/main/scala/mql/model/semantic/AliasedColumn.scala
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2012 Petr Kozelek <petr.kozelek@gmail.com>
+ *
+ * The full copyright and license information is presented
+ * in the file LICENSE that was distributed with this source code.
+ */
 package mql.model.semantic
 
 object AliasedColumnCompanion {
