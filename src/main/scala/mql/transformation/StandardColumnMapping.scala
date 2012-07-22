6c4b4af46e0efeec8090abd042c1671eaf850c9a
Petr Kozelek <petr.kozelek@gmail.com>
2012-07-22 23:19:54 +0200

    Unified ColumnTransformation with CommandExpression. CommandExpression is now used in both column transformations and referenced sources conditions.

--- src/main/scala/mql/transformation/StandardColumnMapping.scala
+++ src/main/scala/mql/transformation/StandardColumnMapping.scala
@@ -8,12 +8,12 @@ package mql.transformation
 
 import mql.model.semantic._
 
-object StandardColumnTransformation {
+object StandardColumnMapping {
   def oneToOne(from: AliasedColumn, to: Column): ColumnMapping = {
     new ColumnMapping {
       column = to
       mappedTo += from.column.entity
-      transformation = NoTransformation(from)
+      transformation = ColumnExpression(from)
     }
   }
 
@@ -22,7 +22,7 @@ object StandardColumnTransformation {
       column = column1
       mappedTo += from.column.entity
       mappedTo += to.column.entity
-      transformation = TokenizedTransformation("", 'from -> from, 'to -> to)
+      transformation = TokenizedExpression("", 'from -> from, 'to -> to)
     }
   }
 }
